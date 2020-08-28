23b0090a671c5cd02d3fe60c5e3804688d8ebe20
Riaz11-coder <riaz11@hotmail.com>
2020-08-28 15:01:07 -0400

    commit2

--- src/test/java/Day07_/LibraryApp_API_DB_Test.java
+++ src/test/java/Day07_/LibraryApp_API_DB_Test.java
@@ -1,18 +1,32 @@
 package Day07_;
 
+import Utility.LibraryUtil;
+import org.junit.jupiter.api.Test;
+
 import Utility.ConfigurationReader;
 import Utility.DB_Utility;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class LibraryApp_API_DB_Test {
+
+    private static String libraryToken;
 
     @BeforeAll
     public static void setUp(){
         RestAssured.baseURI = ConfigurationReader.getProperty("baseURL");
         RestAssured.basePath = "/rest/v1";
         DB_Utility.createConnection("library1");
+        libraryToken = LibraryUtil.loginAndGetToken(ConfigurationReader.getProperty("library1.librarian_username"),
+        ConfigurationReader.getProperty("library1.librarian_password"));
+    }
+
+
+    @Test
+    public void test(){
+        System.out.println("libraryToken = " + libraryToken);
     }
 
     @AfterAll
