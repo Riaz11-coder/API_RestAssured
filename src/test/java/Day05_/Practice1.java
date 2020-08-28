54f9e33974e05ddb6cda4e534891f2a9293f7070
Riaz11-coder <riaz11@hotmail.com>
2020-08-28 11:25:51 -0400

    Day 6

--- src/test/java/Day05_/Practice1.java
+++ src/test/java/Day05_/Practice1.java
@@ -28,10 +28,13 @@ import static org.hamcrest.Matchers.*;
 
 public class Practice1 {
 
+    private static String libraryToken ;
+
     @BeforeAll
     public static void setUp() {
         baseURI = "http://library1.cybertekschool.com";
         basePath = "/rest/v1";
+        libraryToken = LoginAndGetToken("librarian69@library","KNPXrm3S");
 
     }
 
@@ -76,6 +79,66 @@ public class Practice1 {
 
         System.out.println(LoginAndGetToken("librarian69@library","KNPXrm3S"));
     }
+
+    @Test
+    public void testlibraryApp(){
+
+         given()
+                .log().all()
+                .header("x-library-token",libraryToken).
+                when()
+                .get("/dashboard_stats").
+                then()
+                .log().all()
+                .statusCode(200)
+                .body("book_count",is("985"))
+                .body("borrowed_books",is("600"))
+                .body("users",is("5042"));
+
+
+
+    }
+
+    @Test
+    public void testDecode(){
+
+        given()
+                .log().all()
+                .contentType(ContentType.URLENC)
+                .formParams("token",libraryToken).
+                when()
+                .post("/decode").
+                then()
+                .log().all()
+                .statusCode(200)
+                .body("id",is("2090"))
+                .body("full_name",is("Test Librarian 69"))
+                .body("email",is("librarian69@library"))
+                .body("user_group_id",is("2"));
+    }
+
+    @Test
+    public void testGetId(){
+
+        given()
+                .log().all()
+                .header("x-library-token",libraryToken)
+                .accept(ContentType.JSON)
+                .pathParam("id",2080).
+                when()
+                .get("/get_user_by_id/{id}").
+                then()
+                .log().all()
+                .statusCode(is(200))
+                .body("id",is("2080"))
+                .body("full_name",is("Test Student 142"))
+                .body("user_group_id",is("3"))
+                .body("image",is(nullValue()));
+
+
+    }
+
+
 
 
 
